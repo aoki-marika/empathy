013cbdc7656cb57170d9e3a9a5859eaf5209c8b9
marika <marika@waifu.club>
2020-07-23 07:26:10 -0300

    Add sampler2D uniform setting to program.

--- include/core/program.h
+++ include/core/program.h
@@ -1,10 +1,16 @@
 #pragma once
 
 #include "shader.h"
+#include "texture.h"
 
 ///
 /// Programs are used to manage OpenGL shader programs.
 ///
+
+// MARK: - Macros
+
+/// The maximum number of uniform locations that a single program can cache concurrently.
+#define PROGRAM_MAX_CACHED_UNIFORMS 6
 
 // MARK: - Data Structures
 
@@ -13,6 +19,21 @@ struct program_t
 {
     /// Unique OpenGL identifier of this program's backing.
     GLuint id;
+
+    /// The total number of the currently cached uniform locations within this program.
+    unsigned int num_cached_uniforms;
+
+    /// All the currently cached uniform locations within this program.
+    struct program_uniform_t
+    {
+        /// The name of this uniform within the program.
+        ///
+        /// Allocated.
+        char *name;
+
+        /// The unique OpenGL identifier of this uniform's location within it's program.
+        GLuint location;
+    } cached_uniforms[PROGRAM_MAX_CACHED_UNIFORMS];
 };
 
 // MARK: - Functions
@@ -37,3 +58,13 @@ void program_deinit(struct program_t *program);
 /// This must be called again if another program is set to be used.
 /// @param program The program to use.
 void program_use(struct program_t *program);
+
+/// Set the given named sampler2D uniform of the given program to the given texture.
+///
+/// The given program is set to be used during this function.
+/// @param program The program to set the uniform of.
+/// @param name The name of the sampler2D uniform to set.
+/// @param texture The texture to set the given sampler2D uniform to.
+void program_set_sampler2d(struct program_t *program,
+                           const char *name,
+                           const struct texture_t *texture);
--- src/core/program.c
+++ src/core/program.c
@@ -2,8 +2,54 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // MARK: - Functions
+
+/// Attempt to locate the given named uniform within the given program, and return the result.
+/// @param program The program to locate the uniform within.
+/// @param name The name of the uniform to locate.
+/// @return The unique OpenGL identifier of the given uniform's location within the given program.
+/// If the result is less than zero then the uniform could not be located.
+GLint program_locate_uniform(struct program_t *program, const char *name)
+{
+    // check the cache to see if the given uniform was already located
+    for (int i = 0; i < program->num_cached_uniforms; i++)
+    {
+        struct program_uniform_t *uniform = &program->cached_uniforms[i];
+        if (strcmp(uniform->name, name) == 0)
+            return uniform->location;
+    }
+
+    // get the location, returning early if there was an error
+    GLint location = glGetUniformLocation(program->id, name);
+    if (location < 0)
+        return location;
+
+    // ensure the cache isnt full, if it is then clear it
+    if (program->num_cached_uniforms + 1 > PROGRAM_MAX_CACHED_UNIFORMS)
+    {
+        for (int i = 0; i < program->num_cached_uniforms; i++)
+            free(program->cached_uniforms[i].name);
+
+        program->num_cached_uniforms = 0;
+    }
+
+    // cache the location
+    // the string needs to be copied to an allocated region first to allow access later
+    // strlen is incremented to account for the terminator character
+    size_t name_size = strlen(name) + 1;
+    char *name_copy = (char *)malloc(name_size * sizeof(char));
+    strcpy(name_copy, name);
+
+    struct program_uniform_t uniform;
+    uniform.name = name_copy;
+    uniform.location = location;
+    program->cached_uniforms[program->num_cached_uniforms++] = uniform;
+
+    // return the uniforms location
+    return location;
+}
 
 void program_init(struct program_t *program,
                   unsigned int num_shaders,
@@ -34,14 +80,36 @@ void program_init(struct program_t *program,
 
     // initialize the program
     program->id = id;
+    program->num_cached_uniforms = 0;
 }
 
 void program_deinit(struct program_t *program)
 {
+    for (int i = 0; i < program->num_cached_uniforms; i++)
+        free(program->cached_uniforms[i].name);
+
     glDeleteProgram(program->id);
 }
 
 void program_use(struct program_t *program)
 {
     glUseProgram(program->id);
+}
+
+void program_set_sampler2d(struct program_t *program,
+                           const char *name,
+                           const struct texture_t *texture)
+{
+    // the program needs to be in use to set uniforms
+    // just to keep consistency its set regardless of whether or not the uniform is located
+    program_use(program);
+
+    // locate the uniform, returning early if it cannot be located
+    GLint location = program_locate_uniform(program, name);
+    if (location < 0)
+        return;
+
+    // set the uniform to the given textures unit
+    // sampler2d uniforms use unit indices instead of enum cases
+    glUniform1i(location, texture->unit - GL_TEXTURE0);
 }
