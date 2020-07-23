6e819b2fb91170e22dff211b1a923f94fb1eb9ec
marika <marika@waifu.club>
2020-07-23 06:28:32 -0300

    Revise the window API to be more friendly and abstract out OpenGL.

--- include/core/window.h
+++ include/core/window.h
@@ -3,14 +3,15 @@
 #include <stdbool.h>
 
 #include "gl.h"
+#include "colour.h"
 
 ///
-/// Windows are used to manage independent desktop windows which contain OpenGL contexts.
+/// Windows are used to manage independent desktop windows which contain graphics contexts.
 ///
 
 // MARK: - Data Structures
 
-/// A window on the desktop containing an OpenGL context.
+/// A window on the desktop containing a graphics context.
 struct window_t
 {
     /// The backing GLFW window of this window.
@@ -21,7 +22,9 @@ struct window_t
 
 /// Initialize the given window with the given parameters.
 ///
-/// The new window will not be resizable, and always contains an OpenGL 3.3 core context.
+/// The new window will not be resizable.
+/// The new window always contains an OpenGL 3.3 core graphics context.
+/// During this function the new window is set as the current window for the calling thread.
 /// @param window The window to initialize.
 /// @param width The width of the new window, in pixels.
 /// @param height The height of the new window, in pixels.
@@ -35,22 +38,36 @@ void window_init(struct window_t *window,
 /// @param window The window to deinitialize.
 void window_deinit(struct window_t *window);
 
-/// Get whether or not the given window should be closed.
+/// Set the given window's graphics context to be current.
 ///
-/// This should be checked at the beginning of each frame of the given window to determine whether or not the window's frame loop should terminate.
+/// This must be called before any graphics-related functions can be used within the given window.
+/// Only one window at a time can be current on a given thread, but several windows can be current across separate threads.
+/// @param window The window containing the graphics context to make current.
+void window_set_current(struct window_t *window);
+
+/// Set the background colour of the given window to the given colour.
+/// @param window The window to set the background colour of.
+/// @param colour The colour to set the background colour to.
+void window_set_background(struct window_t *window, const struct colour_t *colour);
+
+/// Get whether or not the given window was closed by the user.
+///
+/// Window closure is not automatic, this only indicates whether or not the user tried to close the given window.
+/// This should be checked before beginning a new frame to determine whether or not the given window's frame loop should terminate.
 /// `core_poll_events(struct core_t *)` must be called before this value will update.
 /// @param window The window to check.
-/// @return Whether or not the given window should be closed.
-bool window_should_close(struct window_t *window);
+/// @return Whether or not the given window was closed by the user.
+bool window_is_closed(struct window_t *window);
 
-/// Make the given window's OpenGL context current.
+/// Begin a new frame that will be displayed by the given window.
 ///
-/// This must be called before OpenGL can be used with the given window, and must be called again when it is called on another window.
-/// @param window The window containing the OpenGL context to make current.
-void window_make_current(struct window_t *window);
+/// This must be called at the beginning of every frame for the given window.
+/// @param window The window to begin the new frame in.
+void window_begin_frame(struct window_t *window);
 
-/// Swap the double framebuffers of the given window, displaying the newly drawn one, and wait for vertical sync.
+/// End the current frame and display it within the given window.
 ///
-/// This should be called at the end of each frame of the given window, after drawing into the framebuffer.
-/// @param window The window to swap the framebuffers of.
-void window_swap_buffers(struct window_t *window);
+/// This must be called at the end of every frame for the given window.
+/// Once the frame is displayed then this function waits for vertical sync.
+/// @param window The window to display the new frame within.
+void window_end_frame(struct window_t *window);
--- src/core/window.c
+++ src/core/window.c
@@ -13,6 +13,11 @@ void window_init(struct window_t *window,
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     window->backing = glfwCreateWindow(width, height, title, NULL, NULL);
+
+    // configure opengl
+    window_set_current(window);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 }
 
 void window_deinit(struct window_t *window)
@@ -20,17 +25,31 @@ void window_deinit(struct window_t *window)
     glfwDestroyWindow(window->backing);
 }
 
-bool window_should_close(struct window_t *window)
-{
-    return glfwWindowShouldClose(window->backing) == GLFW_TRUE;
-}
-
-void window_make_current(struct window_t *window)
+void window_set_current(struct window_t *window)
 {
     glfwMakeContextCurrent(window->backing);
 }
 
-void window_swap_buffers(struct window_t *window)
+void window_set_background(struct window_t *window, const struct colour_t *colour)
 {
+    window_set_current(window);
+    glClearColor(colour->r, colour->g, colour->b, colour->a);
+}
+
+bool window_is_closed(struct window_t *window)
+{
+    return glfwWindowShouldClose(window->backing) == GLFW_TRUE;
+}
+
+void window_begin_frame(struct window_t *window)
+{
+    // clear the new framebuffer
+    window_set_current(window);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void window_end_frame(struct window_t *window)
+{
+    // display thew new framebuffer
     glfwSwapBuffers(window->backing);
 }
