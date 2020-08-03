721661f4528f1f551b3f5ec4af8de39a9edd659b
marika <marika@waifu.club>
2020-08-03 15:34:58 -0300

    Add pausing support to clock.

--- include/core/clock.h
+++ include/core/clock.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdbool.h>
+
 ///
 /// Clocks are used to track system time with high accuracy, in a self-contained relative time.
 ///
@@ -10,12 +12,18 @@
 struct clock_t
 {
     /// The time, in milliseconds, that this clock uses as its reference point.
-    double start_time;
+    double reference_time;
+
+    /// Whether or not this clock is currently paused.
+    bool is_paused;
+
+    /// The last time, in milliseconds, at which this clock was paused, if any.
+    double pause_time;
 };
 
 // MARK: - Functions
 
-/// Initialize the given clock, setting its start time to the current time.
+/// Initialize the given clock, setting its reference time to the current time.
 /// @param clock The clock to initialize.
 void clock_init(struct clock_t *clock);
 
@@ -28,6 +36,17 @@ void clock_deinit(struct clock_t *clock);
 /// @return The current relative time of the given clock, in milliseconds.
 double clock_get_time(struct clock_t *clock);
 
-/// Reset the start time of the given clock to the current time.
+/// Set whether or not the given clock is paused.
+///
+/// While a clock is paused its relative time is frozen,
+/// then when it is unpaused the relative time resumes from where it was when it was paused.
+/// If the pause state of the given clock matches the given value then this function does nothing.
+/// @param clock The clock to set the pause state of.
+/// @param is_paused Whether the given clock should be paused or unpaused.
+void clock_set_paused(struct clock_t *clock, bool is_paused);
+
+/// Reset the reference time of the given clock to the current time.
+///
+/// If the given clock is paused then it is automatically unpaused.
 /// @param clock The clock to reset.
 void clock_reset(struct clock_t *clock);
--- src/core/clock.c
+++ src/core/clock.c
@@ -14,6 +14,10 @@ double clock_get_absolute_time()
 
 void clock_init(struct clock_t *clock)
 {
+    // initialize the given clock
+    clock->is_paused = false;
+
+    // set the initial reference time
     clock_reset(clock);
 }
 
@@ -23,10 +27,35 @@ void clock_deinit(struct clock_t *clock)
 
 double clock_get_time(struct clock_t *clock)
 {
-    return clock_get_absolute_time() - clock->start_time;
+    // return the relative paused time if the given clock is paused
+    // return the relative time if the given clock is unpaused
+    if (clock->is_paused)
+        return clock->pause_time - clock->reference_time;
+    else
+        return clock_get_absolute_time() - clock->reference_time;
+}
+
+void clock_set_paused(struct clock_t *clock, bool is_paused)
+{
+    // if the given pause state is already set then there is nothing to do
+    if (is_paused == clock->is_paused)
+        return;
+
+    // when the clock is paused store the time so it can be used as the reference time
+    // when the clock is unpaused change the reference time to include the relative time before being paused
+    if (is_paused)
+        clock->pause_time = clock_get_absolute_time();
+    else
+        clock->reference_time = clock_get_absolute_time() - (clock->pause_time - clock->reference_time);
+
+    clock->is_paused = is_paused;
 }
 
 void clock_reset(struct clock_t *clock)
 {
-    clock->start_time = clock_get_absolute_time();
+    // ensure the clock is unpaused
+    if (clock->is_paused)
+        clock_set_paused(clock, false);
+
+    clock->reference_time = clock_get_absolute_time();
 }
