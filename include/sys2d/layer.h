09b20359bb820416fa9ae1c25ecb79f4b69e91e9
marika <marika@waifu.club>
2020-08-09 15:25:01 -0300

    Cleanup various layer function parameters.

--- include/sys2d/layer.h
+++ include/sys2d/layer.h
@@ -99,38 +99,38 @@ void layer_init(struct layer_t *layer,
 /// @param layer The layer to deinitialize.
 void layer_deinit(struct layer_t *layer);
 
-/// Add a new child layer to the given parent layer's children with the given properties and attachments.
-/// @param child_id The pointer to set the value of to the unique identifier of the new child layer, within the given parent's children.
-/// If this is `NULL` then it is not set.
-/// @param parent The layer to add the new child layer to the children of.
+/// Add a new child layer to the given layer's children with the given properties and attachments.
+/// @param layer The layer to add the new child layer to the children of.
 /// It is expected that this layer is available for the entire lifetime of the new child layer.
+/// @param child_id The pointer to set the value of to the unique identifier of the new child layer, within the given layer's children.
+/// If this is `NULL` then it is not set.
 /// @param properties The properties of the new child layer.
 /// @param num_attachments The total number of given attachments.
 /// @param attachments All the attachments to attach to the new child layer.
 /// The elements of this array are copied, so it does not need to remain accessible.
 /// It is expected that the properties of each attachment are available for the entire lifetime of the new child layer.
-void layer_add(layer_id_t *child_id,
-               struct layer_t *parent,
+void layer_add(struct layer_t *layer,
+               layer_id_t *child_id,
                struct layer_properties_t properties,
                unsigned int num_attachments,
                const struct layer_attachment_t *attachments);
 
-/// Remove the first child layer matching the given unique identifier within the given parent's children, deinitializing it.
+/// Remove the first child layer matching the given unique identifier within the given layer's children, deinitializing it.
 ///
-/// If no child matching the given unique identifier is found within the given parent's children then the program terminates.
+/// If no child matching the given unique identifier is found within the given layer's children then the program terminates.
+/// @param layer The layer containing the layer to remove.
 /// @param child_id The unique identifier of the child layer to remove.
-/// @param parent The layer containing the layer to remove.
-void layer_remove(layer_id_t child_id,
-                  struct layer_t *parent);
+void layer_remove(struct layer_t *layer,
+                  layer_id_t child_id);
 
-/// Get the first child layer matching the given unique identifier within the given parent's children, if any.
+/// Get the first child layer matching the given unique identifier within the given layer's children, if any.
+/// @param layer The layer containing the layer to get.
 /// @param child_id The unique identifier of the child layer to get.
-/// @param parent The layer containing the layer to get.
-/// @return A pointer to the first child layer matching the given unique identifier within the given parent's children.
-/// This pointer is only accessible until the given parent's children are modified.
+/// @return A pointer to the first child layer matching the given unique identifier within the given layer's children.
+/// This pointer is only available until the given layer's children are modified.
 /// If no matches were found then `NULL` is returned instead.
-struct layer_t *layer_get(layer_id_t child_id,
-                          struct layer_t *parent);
+struct layer_t *layer_get(struct layer_t *layer,
+                          layer_id_t child_id);
 
 /// Set the properties of the given layer to the given properties.
 /// @param layer The layer to set the properties of.
--- src/sys2d/layer.c
+++ src/sys2d/layer.c
@@ -6,19 +6,19 @@
 
 // MARK: - Functions
 
-/// Get the index of the first child layer matching the given unique identifier within the given parent's children.
+/// Get the index of the first child layer matching the given unique identifier within the given layer's children.
 /// @param child_id The unique identifier of the child layer to get the index of.
-/// @param parent The layer containing the layer to get the index of.
-/// @return The index of the first child layer matching the given unique identifier within the given parent's children.
+/// @param layer The layer containing the layer to get the index of.
+/// @return The index of the first child layer matching the given unique identifier within the given layer's children.
 /// If no matches were found then `-1` is returned instead.
-int layer_get_child_index(layer_id_t child_id,
-                          struct layer_t *parent)
+int layer_get_child_index(struct layer_t *layer,
+                          layer_id_t child_id)
 {
-    // attempt to return the index of the first layer matching the given id within the given parents children
-    for (int i = 0; i < parent->num_children; i++)
+    // attempt to return the index of the first layer matching the given id within the given layers children
+    for (int i = 0; i < layer->num_children; i++)
     {
-        struct layer_t *child = &parent->children[i];
-        if (child->id == child_id)
+        struct layer_t *child_layer = &layer->children[i];
+        if (child_layer->id == child_id)
             return i;
     }
 
@@ -54,64 +54,63 @@ void layer_deinit(struct layer_t *layer)
     free(layer->attachments);
 }
 
-void layer_add(layer_id_t *child_id,
-               struct layer_t *parent,
+void layer_add(struct layer_t *layer,
+               layer_id_t *child_id,
                struct layer_properties_t properties,
                unsigned int num_attachments,
                const struct layer_attachment_t *attachments)
 {
     // insert the new child layer
-    unsigned int layer_index = parent->num_children;
-    parent->num_children++;
-    parent->children = realloc(parent->children,
-                               parent->num_children * sizeof(struct layer_t));
+    unsigned int child_index = layer->num_children++;
+    layer->children = realloc(layer->children,
+                              layer->num_children * sizeof(struct layer_t));
 
-    struct layer_t *layer = &parent->children[layer_index];
+    struct layer_t *child_layer = &layer->children[child_index];
 
     // initialize the new child layer
-    layer_init(layer,
+    layer_init(child_layer,
                properties,
                num_attachments,
                attachments);
-    layer->id = parent->next_child_id++;
+    child_layer->id = layer->next_child_id++;
 
     // set the given child id pointers value, if there is one
     if (child_id != NULL)
-        *child_id = layer->id;
+        *child_id = child_layer->id;
 }
 
-void layer_remove(layer_id_t child_id,
-                  struct layer_t *parent)
+void layer_remove(struct layer_t *layer,
+                  layer_id_t child_id)
 {
-    // attempt to get the index of the layer to remove
-    int index = layer_get_child_index(child_id, parent);
-    if (index < 0)
+    // attempt to get the index of the child layer to remove
+    int child_index = layer_get_child_index(layer, child_id);
+    if (child_index < 0)
     {
         // the child layer could not be found, print the details and terminate
-        fprintf(stderr, "LAYER ERROR: could not locate child layer %u within layer %p\n", child_id, parent);
+        fprintf(stderr, "LAYER ERROR: could not locate child layer %u within layer %p\n", child_id, layer);
         exit(EXIT_FAILURE);
     }
 
     // deinit the child layer before removing it
-    struct layer_t *layer = &parent->children[index];
-    layer_deinit(layer);
+    struct layer_t *child_layer = &layer->children[child_index];
+    layer_deinit(child_layer);
 
-    // shuffle and reallocate the parents children array to remove the child layers element
-    memmove(&parent->children[index],
-            &parent->children[index + 1],
-            (parent->num_children - (index + 1)) * sizeof(struct layer_t));
+    // shuffle and reallocate the layers children array to remove the child layers element
+    memmove(&layer->children[child_index],
+            &layer->children[child_index + 1],
+            (layer->num_children - (child_index + 1)) * sizeof(struct layer_t));
 
-    parent->num_children--;
-    parent->children = realloc(parent->children,
-                               parent->num_children * sizeof(struct layer_t));
+    layer->num_children--;
+    layer->children = realloc(layer->children,
+                              layer->num_children * sizeof(struct layer_t));
 }
 
-struct layer_t *layer_get(layer_id_t child_id,
-                                struct layer_t *parent)
+struct layer_t *layer_get(struct layer_t *layer,
+                          layer_id_t child_id)
 {
-    int index = layer_get_child_index(child_id, parent);
-    if (index >= 0)
-        return &parent->children[index];
+    int child_index = layer_get_child_index(layer, child_id);
+    if (child_index >= 0)
+        return &layer->children[child_index];
     else
         return NULL;
 }
