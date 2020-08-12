52f77d60d0a2683cb87e26fbe5d5457ac8d58ed9
marika <marika@waifu.club>
2020-08-12 14:11:19 -0300

    Add attachment to Sys2D to split out layer attachment functionality.

---
+++ include/sys2d/attachment.h
@@ -0,0 +1,145 @@
+#pragma once
+
+#include <core/colour.h>
+#include <core/uv.h>
+#include <core/mesh.h>
+#include <core/vector.h>
+
+///
+/// Attachments define different types of visual contents for layers.
+///
+/// An attachment can be one of several types:
+///  - Colour: The attachment draws a quad with colours in each corner, interpolating between them.
+///  - Texture: The attachment draws a quad with a texture on it, sampling the texture within UV bounds.
+///
+/// Attachments follow the same rendering system as layers, re-rendering as little as possible and using dirt to track changes affecting rendered state.
+/// See `layer.h` for further documentation on this.
+///
+
+// MARK: - Macros
+
+/// The index of the vertex attribute that the rendered meshes of attachments bind their XYZ positions to.
+///
+/// XYZ coordinates are in pixels, with a top-left origin.
+#define ATTACHMENT_XYZ_ATTRIBUTE_INDEX           (0)
+
+/// The index of the vertex attribute that the rendered meshes of attachments bind their RGBA colours to.
+///
+/// RGBA components are normalized from `0` to `1`.
+#define ATTACHMENT_RGBA_ATTRIBUTE_INDEX          (1)
+
+/// The index of the vertex attribute that the rendered meshes of attachments bind their texture UV coordinates to.
+///
+/// UV components are normalized from `0` to `1`, with a bottom-left origin.
+#define ATTACHMENT_UV_ATTRIBUTE_INDEX            (2)
+
+/// The index of the vertex attribute that the rendered meshes of attachments bind their array texture index to.
+///
+/// Array texture indices are in elements.
+#define ATTACHMENT_TEXTURE_INDEX_ATTRIBUTE_INDEX (3)
+
+// MARK: - Data Structures
+
+/// A single attachment.
+struct attachment_t
+{
+    /// The type of this attachment.
+    enum attachment_type_t
+    {
+        /// The attachment draws colours on the four corners of a quad, interpolating between them.
+        ATTACHMENT_COLOUR = 0x0,
+
+        /// The attachment draws a texture on a quad, sampling it using UV bounds.
+        ATTACHMENT_TEXTURE = 0x1,
+    } type;
+
+    /// All the properties of this attachment affected by changes which have occurred within this attachment since the last render pass.
+    enum attachment_dirt_t
+    {
+        /// Mesh.
+        ATTACHMENT_MESH = 1 << 0,
+    } dirt;
+
+    /// The backing colour attachment of this attachment, if any.
+    ///
+    /// This is only used if `type` is `ATTACHMENT_COLOUR`.
+    struct attachment_colour_t
+    {
+        /// The colour of the top-left corner of this attachment.
+        struct colour4_t top_left;
+
+        /// The colour of the top-right corner of this attachment.
+        struct colour4_t top_right;
+
+        /// The colour of the bottom-left corner of this attachment.
+        struct colour4_t bottom_left;
+
+        /// The colour of the bottom-right corner of this attachment.
+        struct colour4_t bottom_right;
+    } colour;
+
+    /// The backing texture attachment of this attachment, if any.
+    ///
+    /// This is only used if `type` is `ATTACHMENT_TEXTURE`.
+    struct attachment_texture_t
+    {
+        /// The texture of which this attachment samples.
+        ///
+        /// The lifetime of this texture is handled by the creator of this attachment.
+        struct texture_t *source;
+
+        /// The index, within this attachment's array texture, of the element that this attachment samples.
+        ///
+        /// This is only used if `texture` is of type `TEXTURE_2D_ARRAY`.
+        unsigned int source_index;
+
+        /// The bottom-left UV coordinates of the bounds that this attachment samples its texture from.
+        struct uv_t bottom_left;
+
+        /// The top-right UV coordinates of the bounds that this attachment samples its texture from.
+        struct uv_t top_right;
+    } texture;
+
+    /// The last rendered state of this attachment.
+    struct attachment_rendered_state_t
+    {
+        /// The mesh of the attachment, if any.
+        ///
+        /// Only size is accounted for in this mesh; positional or otherwise properties must be handled by the drawer.
+        /// Allocated.
+        struct mesh_t *mesh;
+    } rendered_state;
+};
+
+// MARK: - Functions
+
+/// Initialize the given attachment as a colour attachment with the given properties.
+/// @param attachment The attachment to initialize.
+/// See `attachment_colour_t` for property documentation.
+void attachment_init_colour(struct attachment_t *attachment,
+                            struct colour4_t top_left,
+                            struct colour4_t top_right,
+                            struct colour4_t bottom_left,
+                            struct colour4_t bottom_right);
+
+/// Initialize the given attachment as a texture attachment with the given properties.
+/// @param attachment The attachment to initialize.
+/// See `attachment_texture_t` for property documentation.
+void attachment_init_texture(struct attachment_t *attachment,
+                             struct texture_t *source,
+                             unsigned int source_index,
+                             struct uv_t bottom_left,
+                             struct uv_t top_right);
+
+/// Deinitialize the given attachment, releasing all of its allocated resources.
+/// @param attachment The attachment to deinitialize.
+void attachment_deinit(struct attachment_t *attachment);
+
+/// Perform a render pass on the given attachment, re-rendering any properties that have changed since the last render pass.
+///
+/// This must be called before a mesh is used, and after properties have been set.
+/// Meshes on their own will not perform a render pass, which will leave the rendered state empty.
+/// @param attachment The attachment to render.
+/// @param size The size of the given attachment, in pixels.
+void attachment_render(struct attachment_t *attachment,
+                       struct vector2_t size);
---
+++ src/sys2d/attachment.c
@@ -0,0 +1,208 @@
+#include "attachment.h"
+
+#include <stdlib.h>
+
+// MARK: - Functions
+
+/// Initialize the given mesh with the current state of the given colour attachment at the given size.
+/// @param attachment The colour attachment to render.
+/// @param size The size to render the given colour attachment at, in pixels.
+/// @param mesh The mesh to initialize.
+void attachment_colour_render_mesh(struct attachment_colour_t *attachment,
+                                   struct vector2_t size,
+                                   struct mesh_t *mesh)
+{
+    // create the mesh
+    // components
+    static const struct mesh_component_t components[] =
+    {
+        {
+            .attribute_index = ATTACHMENT_XYZ_ATTRIBUTE_INDEX,
+            .num_values = 3,
+            .value_type = MESH_COMPONENT_F32,
+            .padding = 0,
+        },
+        {
+            .attribute_index = ATTACHMENT_RGBA_ATTRIBUTE_INDEX,
+            .num_values = 4,
+            .value_type = MESH_COMPONENT_F32,
+            .padding = 0,
+        },
+    };
+
+    // vertices
+    // use temporary shorthands to make the initializer shorter
+    float w = size.x;
+    float h = size.y;
+    struct colour4_t tl = attachment->top_left;
+    struct colour4_t tr = attachment->top_right;
+    struct colour4_t bl = attachment->bottom_left;
+    struct colour4_t br = attachment->bottom_right;
+    const float vertices[] =
+    {
+        0, 0, 0,    tl.r, tl.g, tl.b, tl.a, //top-left
+        w, 0, 0,    tr.r, tr.g, tr.b, tr.a, //top-right
+        0, h, 0,    bl.r, bl.g, bl.b, bl.a, //bottom-left
+        w, h, 0,    br.r, br.g, br.b, br.a, //bottom-right
+    };
+
+    // indices
+    // use two triangles to form a quad
+    const unsigned int indices[] =
+    {
+        0, 1, 2, //top-left triangle
+        1, 2, 3, //bottom-right triangle
+    };
+
+    // initialize the mesh
+    mesh_init(mesh,
+              sizeof(components) / sizeof(struct mesh_component_t),
+              components,
+              sizeof(vertices),
+              vertices,
+              sizeof(indices),
+              indices);
+}
+
+/// Initialize the given mesh with the current state of the given texture attachment at the given size.
+/// @param attachment The texture attachment to render.
+/// @param size The size to render the given texture attachment at, in pixels.
+/// @param mesh The mesh to initialize.
+void attachment_texture_render_mesh(struct attachment_texture_t *attachment,
+                                    struct vector2_t size,
+                                    struct mesh_t *mesh)
+{
+    // create the mesh
+    // components
+    // texture index is always passed regardless of whether or not it is used, to simplify mesh creation
+    static const struct mesh_component_t components[] =
+    {
+        {
+            .attribute_index = ATTACHMENT_XYZ_ATTRIBUTE_INDEX,
+            .num_values = 3,
+            .value_type = MESH_COMPONENT_F32,
+            .padding = 0,
+        },
+        {
+            .attribute_index = ATTACHMENT_UV_ATTRIBUTE_INDEX,
+            .num_values = 2,
+            .value_type = MESH_COMPONENT_F32,
+            .padding = 0,
+        },
+        {
+            .attribute_index = ATTACHMENT_TEXTURE_INDEX_ATTRIBUTE_INDEX,
+            .num_values = 1,
+            .value_type = MESH_COMPONENT_F32,
+            .padding = 0,
+        },
+    };
+
+    // vertices
+    // use temporary shorthands to make the initializer shorter
+    float w = size.x;
+    float h = size.y;
+    struct uv_t bl = attachment->bottom_left;
+    struct uv_t tr = attachment->top_right;
+    unsigned int si = attachment->source_index;
+    const float vertices[] =
+    {
+        0, 0, 0,    bl.u, tr.v,    si, //top-left
+        w, 0, 0,    tr.u, tr.v,    si, //top-right
+        0, h, 0,    bl.u, bl.v,    si, //bottom-left
+        w, h, 0,    tr.u, bl.v,    si, //bottom-right
+    };
+
+    // indices
+    // use two triangles to form a quad
+    const unsigned int indices[] =
+    {
+        0, 1, 2, //top-left triangle
+        1, 2, 3, //bottom-right triangle
+    };
+
+    // initialize the mesh
+    mesh_init(mesh,
+              sizeof(components) / sizeof(struct mesh_component_t),
+              components,
+              sizeof(vertices),
+              vertices,
+              sizeof(indices),
+              indices);
+}
+
+void attachment_init_colour(struct attachment_t *attachment,
+                            struct colour4_t top_left,
+                            struct colour4_t top_right,
+                            struct colour4_t bottom_left,
+                            struct colour4_t bottom_right)
+{
+    // initialize the given attachment
+    attachment->type = ATTACHMENT_COLOUR;
+    attachment->dirt = ATTACHMENT_MESH;
+    attachment->colour.top_left = top_left;
+    attachment->colour.top_right = top_right;
+    attachment->colour.bottom_left = bottom_left;
+    attachment->colour.bottom_right = bottom_right;
+    attachment->rendered_state.mesh = NULL;
+}
+
+void attachment_init_texture(struct attachment_t *attachment,
+                             struct texture_t *source,
+                             unsigned int source_index,
+                             struct uv_t bottom_left,
+                             struct uv_t top_right)
+{
+    // initialize the given attachment
+    attachment->type = ATTACHMENT_TEXTURE;
+    attachment->dirt = ATTACHMENT_MESH;
+    attachment->texture.source = source;
+    attachment->texture.source_index = source_index;
+    attachment->texture.bottom_left = bottom_left;
+    attachment->texture.top_right = top_right;
+    attachment->rendered_state.mesh = NULL;
+}
+
+void attachment_deinit(struct attachment_t *attachment)
+{
+    struct mesh_t *mesh = attachment->rendered_state.mesh;
+    if (mesh != NULL)
+    {
+        mesh_deinit(mesh);
+        free(mesh);
+    }
+}
+
+void attachment_render(struct attachment_t *attachment,
+                       struct vector2_t size)
+{
+    // render the given attachment, depending on its dirt
+    enum attachment_dirt_t dirt = attachment->dirt;
+    if (dirt & ATTACHMENT_MESH)
+    {
+        // get the mesh to initialize
+        // if the mesh is already allocated then it only needs to be reinitialized
+        struct mesh_t **mesh = &attachment->rendered_state.mesh;
+        if (*mesh != NULL)
+            mesh_deinit(*mesh);
+        else
+            *mesh = malloc(sizeof(struct mesh_t));
+
+        // initialize the mesh with the current state of the given attachment
+        switch (attachment->type)
+        {
+            case ATTACHMENT_COLOUR:
+                attachment_colour_render_mesh(&attachment->colour,
+                                              size,
+                                              *mesh);
+                break;
+            case ATTACHMENT_TEXTURE:
+                attachment_texture_render_mesh(&attachment->texture,
+                                               size,
+                                               *mesh);
+                break;
+        }
+    }
+
+    // reset the given attachments dirt, as it has now been applied
+    attachment->dirt = 0x0;
+}
